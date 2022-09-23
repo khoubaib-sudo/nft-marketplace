f68fc889f416e9dcbfa9da24ba2d18c89161a26f
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2022-09-23 15:14:24 +0200

    Active Link

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export {default as Navbar } from "./navbar";
 export {default as BaseLayout} from "./layout/BaseLayout"
 export {default as NftList} from "./nft/list"
 export {default as Nftitem} from "./nft/item"
+export {default as ActiveLink} from "./link"
--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -4,6 +4,7 @@
 import { Disclosure, Menu } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
+import ActiveLink from '../link'
 
 const navigation = [
   { name: 'Marketplace', href: '/', current: true },
@@ -49,20 +50,18 @@ export default function Navbar() {
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <Link 
+                      <ActiveLink
                         key={item.name} 
                         href={item.href}
+                        activeClass="bg-gray-900 text-white"
                         >
                         <a
-                          className={classNames(
-                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                            'px-3 py-2 rounded-md text-sm font-medium'
-                          )}
+                          className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                           aria-current={item.current ? 'page' : undefined}
                         >
                           {item.name}
                         </a>
-                      </Link>
+                      </ActiveLink>
                     ))}
                   </div>
                 </div>
@@ -93,7 +92,6 @@ export default function Navbar() {
                         {({ active }) => (
                           <Link href="/profile">
                             <a
-                              
                               className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                             >
                               Your Profile
