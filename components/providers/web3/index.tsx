27b96fbd6e200de3c823d25ca0565ebc1f95f9ed
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2022-09-24 18:50:02 +0200

    Web3 Types

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -1,13 +1,23 @@
 import { createContext, FunctionComponent, useState, useContext} from "react";
+import { MetaMaskInpageProvider } from "@metamask/providers";
+import { Contract, providers } from "ethers";
 
-const Web3Context = createContext<any>(null);
+
+
+export type web3State = {
+    ethereum?: MetaMaskInpageProvider;
+    provider?: providers.Web3Provider;
+    contract?: Contract;   
+}
+
+const Web3Context = createContext<web3State>(null);
 
 type Web3Providerprops = {
     children: React.ReactNode; //👈 children prop type
 };
 
 const Web3Provider = ({children} : Web3Providerprops) => {
-    const [web3Api , setWeb3Api] = useState({test: "hello provider!"})
+    const [web3Api , setWeb3Api] = useState<web3State>(null)
     return(
         <Web3Context.Provider value = {web3Api}>
             {children}
