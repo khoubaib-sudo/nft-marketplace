c116f2f2ec1c6ac9f48d3625172e1dc22745a2c2
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2022-10-12 11:33:00 +0200

    puf

--- components/ui/navbar/index.tsx
+++ components/ui/navbar/index.tsx
@@ -4,6 +4,7 @@
 import { Disclosure, Menu } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { useAccount } from '@hooks';
+import { isValidAddress } from 'ethereumjs-util';
 import Link from 'next/link'
 import ActiveLink from '../link'
 
@@ -21,11 +22,12 @@ export default function Navbar() {
 
   const { data } = useAccount("Some Random Params");
   console.log(data);
-  
+
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
+        
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
