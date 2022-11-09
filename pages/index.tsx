fcb8dd3fdf216144892410581514d609b4bf39f2
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2022-11-09 21:13:09 +0100

    Use listed Nfts

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@ import type { NextPage } from 'next';
 import { NftList, BaseLayout } from '@ui';
 import nfts from "../content/meta.json"
 import { NftMeta } from '@_types/nft';
-import { useWeb3 } from '@providers/web3';
+import { useListedNfts } from '@hooks/web3';
 
 
 
 const Home: NextPage = () => {
-  const {provider, contract} = useWeb3();
+  const { nfts } = useListedNfts();
+  
+  console.log(nfts.data);
+  
   return (
     <BaseLayout>
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
@@ -22,7 +25,7 @@ const Home: NextPage = () => {
             </p>
           </div>
           <NftList 
-            nfts={nfts as NftMeta []}
+            nfts={nfts?.data}
           />
         </div>
       </div>
