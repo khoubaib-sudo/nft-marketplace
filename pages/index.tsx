db508205099d9a304413d2072aa4c84fc563c531
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2022-11-13 17:09:12 +0100

    buy Nft in hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,8 @@
 
 import type { NextPage } from 'next';
 import { BaseLayout, NftList } from '@ui';
-import { useListedNfts } from '@hooks/web3';
-import { Nft } from '@_types/nft';
 
 const Home: NextPage = () => {
-  const { nfts } = useListedNfts();
   return (
     <BaseLayout>
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
@@ -20,9 +17,7 @@ const Home: NextPage = () => {
               Mint a NFT to get unlimited ownership forever!
             </p>
           </div>
-          <NftList
-            nfts={nfts.data as Nft[]}
-          />
+          <NftList />
         </div>
       </div>
     </BaseLayout>
