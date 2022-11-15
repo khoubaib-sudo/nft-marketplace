0589ba4478cbad140e7205337e8fe87c7c3a1677
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2022-11-15 23:39:55 +0100

    store image to state

--- types/nft.ts
+++ types/nft.ts
@@ -29,4 +29,11 @@ export type FileReq = {
     bytes: Uint8Array;
     contentType: string;
     fileName: string;
+}
+
+export type PinataRes = {
+    IpfsHash: string;
+    PinSize: number;
+    Timestamp: string;
+    isDuplicate: boolean;
   }
