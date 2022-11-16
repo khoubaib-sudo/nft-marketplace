e04309d9453abcc02f0fc48427f6f4c136cc48f0
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2022-11-16 11:01:02 +0100

    Done  ✔

--- pages/nft/create.tsx
+++ pages/nft/create.tsx
@@ -8,11 +8,13 @@ import axios from 'axios';
 import { useWeb3 } from '@providers/web3';
 import { ethers } from 'ethers';
 import { toast } from "react-toastify";
+import { useNetwork } from '@hooks/web3';
 
 const ALLOWED_FIELDS = ["name", "description", "image", "attributes"];
 
 const NftCreate: NextPage = () => {
   const {ethereum, contract} = useWeb3();
+  const {network} = useNetwork();
   const [nftURI, setNftURI] = useState("");
   const [price, setPrice] = useState("");
   const [hasURI, setHasURI] = useState(false);
@@ -148,6 +150,28 @@ const NftCreate: NextPage = () => {
     } catch(e: any) {
       console.error(e.message);
     }
+  }
+  if (!network.isConnectedToNetwork) {
+    return (
+      <BaseLayout>
+        <div className="rounded-md bg-yellow-50 p-4 mt-10">
+          <div className="flex">
+          <div className="py-1"><svg className="fill-current h-6 w-6 text-yellow-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+            <div className="ml-3">
+              <h3 className="text-sm font-medium text-yellow-800">Attention needed</h3>
+              <div className="mt-2 text-sm text-yellow-700">
+                <p>
+                { network.isLoading ?
+                  "Loading..." :
+                  `Connect to ${network.targetNetwork}`
+                }
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </BaseLayout>
+    )
   }
 
   return (
