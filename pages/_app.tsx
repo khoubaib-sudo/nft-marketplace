7ff3c807e8ad27f7771217a6aebdf84690570eff
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2022-11-16 10:26:12 +0100

    handle promises

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Web3Provaider } from '@providers'
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <>
+    <ToastContainer />
     <Web3Provaider>
       <Component {...pageProps}/>
     </Web3Provaider>
