44d6158887878f1f6312984f9182aaa84a80da69
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2022-11-16 10:52:14 +0100

    Display wrong network

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-/* eslint-disable @next/next/no-img-element */
-
 import type { NextPage } from 'next';
 import { BaseLayout, NftList } from '@ui';
+import { useNetwork } from '@hooks/web3';
+// import { ExclamationIcon } from '@heroicons/react/solid';
 
 const Home: NextPage = () => {
+  const { network } = useNetwork();
+
   return (
     <BaseLayout>
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
@@ -12,12 +14,30 @@ const Home: NextPage = () => {
         </div>
         <div className="relative">
           <div className="text-center">
-            <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Amazing Creatures NFTs</h2>
+            <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Amazing Dudes NFTs</h2>
             <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
               Mint a NFT to get unlimited ownership forever!
             </p>
           </div>
-          <NftList />
+          { network.isConnectedToNetwork ?
+            <NftList /> :
+            <div className="rounded-md bg-yellow-50 p-4 mt-10">
+              <div className="flex">
+              <div className="py-1"><svg className="fill-current h-6 w-6 text-yellow-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+                <div className="ml-3">
+                  <h3 className="text-sm font-medium text-yellow-800">Attention needed</h3>
+                  <div className="mt-2 text-sm text-yellow-700">
+                    <p>
+                    { network.isLoading ?
+                      "Loading..." :
+                      `Connect to ${network.targetNetwork}`
+                    }
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          }
         </div>
       </div>
     </BaseLayout>
@@ -25,3 +45,4 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
