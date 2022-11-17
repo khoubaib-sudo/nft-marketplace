3ccdb4825ac3c53808a4fa8afb61ca919beb5675
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2022-11-17 14:45:26 +0100

    final

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider =require("@truffle/hdwallet-provider");
+const keys = require ("./keys.json");
 
 
 module.exports = { 
@@ -8,6 +10,18 @@ module.exports = {
      port: 7545,            
      network_id: "*",       
     },
+    goerli: {
+      provider: () => 
+      new HDWalletProvider(
+        keys.PRIVATE_KEY,
+        keys.INFURA_GOERLI_URL
+      ),
+      network_id: 5,
+      gas: 5500000,
+      gasPrice: 20000000000,
+      confirmations: 2,
+      timeoutBlocks: 200
+    }
   },
   compilers: {
     solc: {
